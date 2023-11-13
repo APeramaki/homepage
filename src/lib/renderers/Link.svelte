0d67c81c8551536939e7e2b258674f4ab4eb7d61
Atte Perämäki <APeramaki@users.noreply.github.com>
2023-11-13 23:34:20 +0200

    Link Element added, codespan corrected to code

---
+++ src/lib/renderers/Link.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+   import {rendermap} from './renderUtil'
+   import type {
+      Tokens,
+   } from 'marked'
+
+   export let token : Tokens.Link;
+</script>
+
+   <a href={token.href} title={token.title}>
+      {#each token.tokens as t }
+         <svelte:component this={rendermap.get(t.type)} token={t} />   
+      {/each}
+   </a>
+
+<style>
+   a {
+      color: var(--link);
+   }
+   a:hover {
+      color: var(--link-hover);
+   }
+   a:active {
+      color: var(--link-active);
+   }
+</style>
--- src/lib/renderers/renderUtil.ts
+++ src/lib/renderers/renderUtil.ts
@@ -1,10 +1,11 @@
 import * as renderers from "$lib/renderers/index";
 export const rendermap = new Map()
    .set("heading", renderers.Heading)
-   .set("code", renderers.Codespan)
+   .set("code", renderers.Code)
    .set("em", renderers.Em)
    .set("paragraph", renderers.Paragraph)
    .set("text", renderers.Text)
    .set("strong", renderers.Strong)
    .set("image", renderers.Image)
+   .set("link", renderers.Link)
    
